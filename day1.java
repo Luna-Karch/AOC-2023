9caaad56a0894db4b25aff79e7802d831d5c4433
Luna-Karch <luna.karch05@gmail.com>
2023-12-04 17:32:22 -0500

    Implemented Part 2 to pass the sample text

--- day1.java
+++ day1.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 
 public class day1 {
     public static HashMap<String, String> numberMapping = new HashMap<String, String>() {{
+        put("zero", "0");
         put("one", "1");
         put("two", "2");
         put("three", "3");
@@ -15,6 +16,16 @@ public class day1 {
         put("seven", "7");
         put("eight", "8");
         put("nine", "9");
+        put("0", "0");
+        put("1", "1");
+        put("2", "2");
+        put("3", "3");
+        put("4", "4");
+        put("5", "5");
+        put("6", "6");
+        put("7", "7");
+        put("8", "8");
+        put("9", "9");
     }};
 
     public static int runPartOne() throws FileNotFoundException {
@@ -61,17 +72,54 @@ public class day1 {
         File inputFile = new File("day1-sample-2.txt");
         Scanner scanner = new Scanner(inputFile);
 
+        int totalValue = 0;
+
         while (scanner.hasNextLine()) {
-            String currentLine = scanner.nextLine();
-            System.out.println(currentLine);
-            // ^^ Just printing the output of each line for now
+            String currentLine = scanner.nextLine();    
+            HashMap<String, Integer> lineStats = new HashMap<String, Integer>();
+
+            numberMapping.forEach((key, value) -> {
+                int numberIndex = currentLine.indexOf(key);
+                
+                if (numberIndex != -1) {
+                    lineStats.put(key, numberIndex);
+                    // ^^ If the word exists in the current line
+                    // ^^ Add it to the temporary mapping
+                }
+            });
+
+            // Walk forwards only this time
+            // Add the numbers found to the lineStats map and take the smallest and largest
+            // To find the first and last number
+
+            for (int i = 0; i < currentLine.length(); i++) {
+                if (Character.isDigit(currentLine.charAt(i))) {
+                    lineStats.put(String.valueOf(currentLine.charAt(i)), i);
+                }
+            }
+
+            String[] minmaxkeys = {"", ""};
+            int[] minmaxnums = {-999999, 999999};
+
+            lineStats.forEach((key, value) -> {
+                if (value < minmaxnums[1]) {
+                    minmaxnums[1] = value;
+                    minmaxkeys[1] = key;
+                }
+
+                if (value > minmaxnums[0]) {
+                    minmaxnums[0] = value;
+                    minmaxkeys[0] = key;
+                }
+            });
+
+            int valueAsInt = Integer.parseInt(numberMapping.get(minmaxkeys[1]) + numberMapping.get(minmaxkeys[0]));
+            totalValue += valueAsInt; 
         }
         scanner.close();
 
-        // Implement Part 2
-        return 0;
+        return totalValue;
     }
-
 
     public static void main(String args[]) throws FileNotFoundException {
         int partOne = runPartOne();

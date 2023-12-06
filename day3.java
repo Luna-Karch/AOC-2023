04fab96ff48ab0336f8adf152e41970ae2f7a804
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 14:22:21 -0500

    Looped through the file, printing each line; added isSymbol function

--- day3.java
+++ day3.java
@@ -1,10 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class day3 {
-    public static int runPartOne() {
-        // Implement
+
+    public static boolean isSymbol(char character) {
+        /* Returns false if the character is a period
+         * or if the character is a digit
+         * otherwise, the character is a symbol, so return true */
+
+        if (character == '.') {
+            return false; 
+        }
+
+        if (Character.isDigit(character)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static int runPartOne() throws FileNotFoundException {
+        File inputFile = new File("day3-sample.txt");
+        Scanner scanner = new Scanner(inputFile);
+
+        while (scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine();
+            System.out.println(currentLine);
+        }
+        scanner.close();
+
         return 0;
     }
 
-    public static void main(String[] args) {
-        // Implement
+    public static void main(String[] args) throws FileNotFoundException {
+        runPartOne();
     }
 }

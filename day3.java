6ffa345d05d219d17a4ae260424a3b7975608d9d
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 16:34:08 -0500

    Printed every line in the file

--- day3.java
+++ day3.java
@@ -1,11 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
 public class day3 {
 
-    public static int runPartOne() {
-        // IMPLEMENT
+    public static int runPartOne() throws FileNotFoundException {
+        File inputFile = new File("day3-sample.txt");
+        Scanner scanner = new Scanner(inputFile);
+        ArrayList<String> fileData = new ArrayList<String>();
+
+        while (scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine();
+            fileData.add(currentLine.trim());
+        }
+
+        for (int i = 0; i < fileData.size(); i++) {
+            System.out.println(fileData.get(i));
+        }
+        
         return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         runPartOne();
     }
 }

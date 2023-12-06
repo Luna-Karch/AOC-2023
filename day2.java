8f58347a7eed0d853f4af9c1c391ff1f25d72676
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 02:01:30 -0500

    Passed the testing case for part 2 with value of 2286

--- day2.java
+++ day2.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashMap;
 import java.io.FileNotFoundException;
 
 public class day2 {
@@ -48,8 +49,50 @@ public class day2 {
         return totalPossible;
     }
 
+    public static int runPartTwo() throws FileNotFoundException {
+        File inputFile = new File("day2-sample.txt");
+        Scanner scanner = new Scanner(inputFile);
+
+        int powerSet = 0;
+
+        while (scanner.hasNextLine()) {
+            HashMap<String, Integer> fewestPossibleCubes = new HashMap<String, Integer>();
+            fewestPossibleCubes.put("red", 0);
+            fewestPossibleCubes.put("green", 0);
+            fewestPossibleCubes.put("blue", 0);
+
+            String currentLine = scanner.nextLine().trim();
+            String[] cubesList = currentLine.split(": ")[1].split(";");
+
+            for (int i = 0; i < cubesList.length; i++) {
+                String cubeConfig = cubesList[i].trim();
+                for (String pulledCubes : cubeConfig.split(", ")) {
+                    String cubeColor = pulledCubes.split(" ")[1];
+                    int cubeAmount = Integer.parseInt(pulledCubes.split(" ")[0]);
+
+                    int currentFewestCubeAmount = fewestPossibleCubes.get(cubeColor);
+
+                    if (currentFewestCubeAmount < cubeAmount) {
+                        fewestPossibleCubes.put(cubeColor, cubeAmount);
+                    }
+                }
+            }
+
+            int subtotal = 1;
+
+            for (String key: fewestPossibleCubes.keySet()) {
+                subtotal *= fewestPossibleCubes.get(key);
+            }
+
+            powerSet += subtotal;
+        }
+        scanner.close();
+
+        return powerSet;
+    }
 
     public static void main(String args[]) throws FileNotFoundException {
         System.out.println(String.format("Part One: %d", runPartOne()));
+        System.out.println(String.format("Part Two: %d", runPartTwo()));
     }
 }

5dddae93d6a5df52e2b41262fcc24b44458b978d
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 01:07:11 -0500

    Started looping through the file and printed every line

--- day2.java
+++ day2.java
@@ -1,10 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class day2 {
-    public static void runPartOne() {
-        // code
+    public static void runPartOne() throws FileNotFoundException {
+        File inputFile = new File("day2-sample.txt");
+        Scanner scanner = new Scanner(inputFile);
+
+        while (scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine();
+            System.out.println(currentLine);
+        }
+        scanner.close();
+
     }
 
 
-    public static void main(String args[]) {
-        // code
+    public static void main(String args[]) throws FileNotFoundException {
+        runPartOne();
     }
 }

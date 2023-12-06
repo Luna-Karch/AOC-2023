a31a0fce041acc8021a226f9bea8e16523b715ba
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 16:09:08 -0500

    Restructured the program entirely, maintained functionality. Started symbol search

--- day3.java
+++ day3.java
@@ -69,51 +69,42 @@ public class day3 {
         return pointsToCheck;
     }
 
-    public static int getLineCount(String path) throws FileNotFoundException {
-        File inputFile = new File(path);
-        Scanner scanner = new Scanner(inputFile);
-
-        int totalLines = 0;
-        while (scanner.hasNextLine()) {
-            scanner.nextLine();
-            totalLines++;
-        }
-
-        scanner.close();
-
-        return totalLines;
-    }
-
     public static int runPartOne() throws FileNotFoundException {
+        // Algorithm plan:
+        // Loop over every line
+        // Loop over every character
+        // Check if you have found a symbol
+        // [DOING] If a symbol was found, check the 9 x 9 area around the symbol for digits
+        // If a digit is found, walk left from the digits index and walk right from the digits index until you have the entire number
+        // Add the number to a sum
+        // return the sum 
+
+
         File inputFile = new File("day3-sample.txt");
         Scanner scanner = new Scanner(inputFile);
-        int totalFileLines = getLineCount("day3-sample.txt");
-
-        int lineCounter = 0;
+        ArrayList<String> fileData = new ArrayList<String>();
+        int totalFileLines = 0;
+        
         while (scanner.hasNextLine()) {
             String currentLine = scanner.nextLine();
+            fileData.add(currentLine);
+            totalFileLines++;
+        }
+        scanner.close();
+
+        for (int i = 0; i < fileData.size(); i++) {
+            String currentLine = fileData.get(i);
             System.out.println(currentLine);
 
             HashMap<Character, Integer> symbolMapping = findSymbols(currentLine);
             
             for (char key: symbolMapping.keySet()) {
-                ArrayList<ArrayList<Integer>> generatedIndices = generateIndices(lineCounter, symbolMapping.get(key), totalFileLines);
-                System.out.println(String.format("Symbol: %c | X: %d | Y: %d", key, lineCounter, symbolMapping.get(key)));
+                ArrayList<ArrayList<Integer>> generatedIndices = generateIndices(i, symbolMapping.get(key), totalFileLines);
+                System.out.println(String.format("Symbol: %c | X: %d | Y: %d", key, i, symbolMapping.get(key)));
                 System.out.println("Generated Indices: ");
                 System.out.println(generatedIndices);
             }
-
-            // Algorithm plan:
-            // Loop over every line
-            // Loop over every character
-            // Check if you have found a symbol
-            // If a symbol was found, check the 9 x 9 area around the sybol for digits
-            // If a digit is found, walk left from the digits index and walk right from the digits index until you have the entire number
-            // Add the number to a sum
-            // return the sum 
-            lineCounter++;
         }
-        scanner.close();
 
         return 0;
     }

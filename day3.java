0de126ac19090f2252f27daf69ca4b14c4367da0
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 18:10:03 -0500

    Successfully added the ability to generate all checkable coordinates

--- day3.java
+++ day3.java
@@ -29,10 +29,10 @@ public class day3 {
                 if (!Character.isDigit(character) && digitMode) {
                     digitMode = false;
 
-                    NumberAndLocation numberValues = new NumberAndLocation(numberBuffer, indexBuffer);
-
+                    NumberAndLocation numberValues = new NumberAndLocation(numberBuffer, indexBuffer, fileData.size(), fileData.get(0).length());
                     System.out.println(numberValues.getValue());
                     System.out.println(numberValues.getCoordinates());
+                    numberValues.checkCoordinates();
                     System.out.println();
 
                     numberBuffer.clear();
@@ -63,9 +63,11 @@ public class day3 {
 
 class NumberAndLocation {
     private int value;
+    private int lineCount;
+    private int lineLength;
     private ArrayList<ArrayList<Integer>> coordinates = new ArrayList<ArrayList<Integer>>();
 
-    public NumberAndLocation (ArrayList<Character> numberBuffer, ArrayList<ArrayList<Integer>> coordinates) {
+    public NumberAndLocation (ArrayList<Character> numberBuffer, ArrayList<ArrayList<Integer>> coordinates, int lineCount, int lineLength) {
         String valueBuffer = "";
         for (char numberPart : numberBuffer) {
             valueBuffer += numberPart;
@@ -73,6 +75,8 @@ class NumberAndLocation {
 
         this.value = Integer.parseInt(valueBuffer);
         this.coordinates = coordinates;
+        this.lineCount = lineCount;
+        this.lineLength = lineLength;
     }
 
     public int getValue() {
@@ -81,6 +85,82 @@ class NumberAndLocation {
 
     public ArrayList<ArrayList<Integer>> getCoordinates() {
         return this.coordinates;
+    }
+
+    private ArrayList<ArrayList<Integer>> getCheckableCoordinates() {
+        // Implement
+        ArrayList<ArrayList<Integer>> result = new  ArrayList<ArrayList<Integer>>();
+
+        ArrayList<Integer> leftmost = this.coordinates.get(0);
+        ArrayList<Integer> rightmost = this.coordinates.get(this.coordinates.size() - 1);
+
+        ArrayList<Integer> topLeftCorner = new ArrayList<Integer>() {{
+            add(leftmost.get(0) - 1);
+            add(leftmost.get(1) - 1);
+        }};
+
+        ArrayList<Integer> topRightCorner = new ArrayList<Integer>() {{
+            add(rightmost.get(0) + 1);
+            add(rightmost.get(1) - 1);
+        }};
+
+        ArrayList<Integer> bottomLeftCorner = new ArrayList<Integer>() {{
+            add(leftmost.get(0) - 1);
+            add(leftmost.get(1) + 1);
+        }};
+
+        ArrayList<Integer> bottomRightCorner = new ArrayList<Integer>() {{
+            add(rightmost.get(0) + 1);
+            add(rightmost.get(1) - 1);
+        }};
+    
+        for (int i = topLeftCorner.get(0); i < topRightCorner.get(0) + 1; i++) {
+            int row = topLeftCorner.get(1);
+            if (i < 0 || row < 0 || i > lineCount - 1 || row > lineCount - 1) {
+                continue;
+            }
+
+            ArrayList<Integer> newPair = new ArrayList<Integer>();
+            newPair.add(i);
+            newPair.add(row);
+
+            result.add(newPair);
+        } // Above, including diagonals, accounting for out of bounds
+
+        for (int i = bottomLeftCorner.get(0); i < bottomRightCorner.get(0) + 1; i++) {
+            int row = bottomLeftCorner.get(1);
+            if (i < 0 || row < 0 || i > lineCount - 1 || row > lineCount - 1) {
+                continue;
+            }
+
+            ArrayList<Integer> newPair = new ArrayList<Integer>();
+            newPair.add(i);
+            newPair.add(row);
+
+            result.add(newPair);
+        } // Below, including diagonals, accounting for out of bounds
+
+        if (leftmost.get(0) - 1 >= 0) {
+            result.add(new ArrayList<Integer>() {{
+                add(leftmost.get(0) - 1);
+                add(leftmost.get(1));
+            }});
+        }
+
+        if (rightmost.get(0) + 1 < this.lineLength - 1) {
+            result.add(new ArrayList<Integer>() {{
+                add(rightmost.get(0) + 1);
+                add(rightmost.get(1));
+            }});
+        }
+
+        return result;
+    }
+
+    public boolean checkCoordinates() {
+        System.out.println("CHECKABLE COORDINATES:");
+        System.out.println(this.getCheckableCoordinates());
+        return true;
     }
 
 }

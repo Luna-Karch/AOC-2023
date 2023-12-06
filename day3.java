f142cbf7dc93af9d811ae25b2e858d73bf406944
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 14:15:13 -0500

    Added template for day 3 with sample text

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class day3 {
+    public static int runPartOne() {
+        // Implement
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Implement
+    }
+}

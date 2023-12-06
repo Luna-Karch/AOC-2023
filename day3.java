499c7a519e607fd6c8c12d921f5d338c60df059f
Luna-Karch <luna.karch05@gmail.com>
2023-12-06 16:01:28 -0500

    Implemented generateIndices function and getLineCount function

--- day3.java
+++ day3.java
@@ -1,6 +1,8 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class day3 {
 
@@ -20,26 +22,96 @@ public class day3 {
         return true;
     }
 
-    public static boolean lineHasSymbol(String line) {
-        // Iterates through a line and determines if any symbols are present
+    public static HashMap<Character, Integer> findSymbols(String line) {
+        // Returns a mapping of the symbol character to the index at which the symbol was found in the line
+        HashMap<Character, Integer> symbolMapping = new HashMap<Character, Integer>();
 
-        boolean symbolFound = false;
         for (int i = 0; i < line.length(); i++) {
             if (isSymbol(line.charAt(i))) {
-                symbolFound = true;
+                symbolMapping.put(line.charAt(i), i);
             }
         }
-        return symbolFound;
 
+        // HashMap will be empty if nothing is found
+
+        return symbolMapping;
+    }
+
+    public static ArrayList<ArrayList<Integer>> generateIndices(int lineCount, int symbolIndex, int totalLines) {
+        // lineCount = X
+        // symbolIndex = Y
+        int totalFileLinesIndex = totalLines - 1;
+
+        ArrayList<ArrayList<Integer>> pointsToCheck = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = lineCount - 1; i < lineCount + 2; i++) {
+            for (int j = symbolIndex - 1; j < symbolIndex + 2; j++) {
+                ArrayList<Integer> currentPair = new ArrayList<Integer>();
+
+                if (i < 0 || i > totalFileLinesIndex) {
+                    continue;
+                } // If the X index is out of range
+
+                if (j < 0 || j > totalFileLinesIndex) {
+                    continue;
+                } // If the Y index is out of range
+
+                if (i == lineCount && j == symbolIndex) {
+                    continue;
+                } // If the point is the point where the symbol is
+
+                currentPair.add(i);
+                currentPair.add(j);
+                pointsToCheck.add(currentPair);
+            }
+        }
+
+        return pointsToCheck;
+    }
+
+    public static int getLineCount(String path) throws FileNotFoundException {
+        File inputFile = new File(path);
+        Scanner scanner = new Scanner(inputFile);
+
+        int totalLines = 0;
+        while (scanner.hasNextLine()) {
+            scanner.nextLine();
+            totalLines++;
+        }
+
+        scanner.close();
+
+        return totalLines;
     }
 
     public static int runPartOne() throws FileNotFoundException {
         File inputFile = new File("day3-sample.txt");
         Scanner scanner = new Scanner(inputFile);
+        int totalFileLines = getLineCount("day3-sample.txt");
 
+        int lineCounter = 0;
         while (scanner.hasNextLine()) {
             String currentLine = scanner.nextLine();
             System.out.println(currentLine);
+
+            HashMap<Character, Integer> symbolMapping = findSymbols(currentLine);
+            
+            for (char key: symbolMapping.keySet()) {
+                ArrayList<ArrayList<Integer>> generatedIndices = generateIndices(lineCounter, symbolMapping.get(key), totalFileLines);
+                System.out.println(String.format("Symbol: %c | X: %d | Y: %d", key, lineCounter, symbolMapping.get(key)));
+                System.out.println("Generated Indices: ");
+                System.out.println(generatedIndices);
+            }
+
+            // Algorithm plan:
+            // Loop over every line
+            // Loop over every character
+            // Check if you have found a symbol
+            // If a symbol was found, check the 9 x 9 area around the sybol for digits
+            // If a digit is found, walk left from the digits index and walk right from the digits index until you have the entire number
+            // Add the number to a sum
+            // return the sum 
+            lineCounter++;
         }
         scanner.close();
 

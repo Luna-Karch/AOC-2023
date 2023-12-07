74db95700a238224e3e1f75a8305d1c488e0ed9d
Luna-Karch <luna.karch05@gmail.com>
2023-12-07 16:11:39 -0500

    Added lineAnalyzer class

--- day4.java
+++ day4.java
@@ -1,5 +1,36 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
 public class day4 {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
+    public static void main(String args[]) throws FileNotFoundException {
+        runPartOne();
+    }
+
+    public static int runPartOne() throws FileNotFoundException {
+        File inputFile = new File("day4-sample.txt");
+        Scanner scanner = new Scanner(inputFile);
+
+        int totalPoints = 0;
+        while (scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine().trim();
+            System.out.println(currentLine);
+        }
+        scanner.close();
+
+        return totalPoints;
+    }
+}
+
+class lineAnalyzer {
+    private String raw;
+
+    public lineAnalyzer(String line) {
+        this.raw = line;
+    }
+
+    public String getRaw() {
+        return this.raw;
     }
 }

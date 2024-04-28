176bf552692a14af04d68ce5b5415964d21d3832
Luna Karch <luna.karch05@gmail.com>
2024-04-28 04:07:30 -0400

    Completed part 1

--- Day 1/Solution.cs
+++ Day 1/Solution.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace Solution {
     public class Solution {
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Part1();
+        }
+
+        public static void Part1() {
+            int total = 0;
+            foreach(string line in File.ReadLines("input.txt")) {
+                int firstInt = -1;
+                int lastInt = -1;
+                foreach (char c in line) {
+                    if (firstInt == -1 && char.IsNumber(c)) {
+                        firstInt = (int)char.GetNumericValue(c);
+                    }
+
+                    if (char.IsNumber(c)) {
+                        lastInt = (int)char.GetNumericValue(c);
+                    }
+                }
+
+                string firstAndLast = "";
+                firstAndLast += firstInt.ToString() + lastInt.ToString();
+                int constructedNumberAsInt = int.Parse(firstAndLast);
+                total += constructedNumberAsInt;
+            }
+            Console.WriteLine(total);
         }
     }
 }

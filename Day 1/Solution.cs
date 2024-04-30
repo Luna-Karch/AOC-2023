5661a18b28424df3ce339e4cf9c97ddfe934773b
Luna Karch <luna.karch05@gmail.com>
2024-04-30 09:59:50 -0400

    Finished Day 1 in C#

--- Day 1/Solution.cs
+++ Day 1/Solution.cs
@@ -32,7 +32,57 @@ namespace Solution {
         }
 
         public static void Part2() {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string, string> tokenMap = new() {
+                {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"},
+                {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"},
+                {"nine", "9"}, {"1", "1"}, {"2", "2"}, {"3", "3"}, {"4", "4"},
+                {"5", "5"}, {"6", "6"}, {"7", "7"}, {"8", "8"}, {"9", "9"} 
+            };
+
+            int total = 0;
+
+            foreach(string line in File.ReadLines("input.txt")) {
+                int first = -1;
+                int last = -1;
+
+                string firstFound = "";
+                string lastFound = "";
+
+                foreach(string token in tokenMap.Keys) {
+                    if (!line.Contains(token)) { continue; }
+
+                    List<int> foundIndices = findAllIndices(token, line);
+
+                    if (first == -1 || first > foundIndices.Min()) {
+                        first = foundIndices.Min();
+                        firstFound = token;
+                    }
+
+                    if (last == -1 || last < foundIndices.Max()) {
+                        last = foundIndices.Max();
+                        lastFound = token;
+                    }
+                }
+
+                int result = int.Parse(tokenMap[firstFound] + tokenMap[lastFound]);
+                total += result;   
+            }
+
+            Console.WriteLine("Part 2: " + total);
+        }
+
+        private static List<int> findAllIndices(string subString, string fullString) {
+            List<int> result = [];
+            
+            int index = fullString.IndexOf(subString);
+
+            while (index != -1) {
+                result.Add(index);
+                index = fullString.IndexOf(subString, index + 1);
+            }
+
+            return result;
         }
     }
 }

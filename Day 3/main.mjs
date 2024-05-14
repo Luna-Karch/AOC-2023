05465074510d6dd43a64b5d6c065d40143360274
Luna Karch <luna.karch05@gmail.com>
2024-05-14 13:10:24 -0400

    Added a function to check a provided charatcer for being a symbol

--- Day 3/main.mjs
+++ Day 3/main.mjs
@@ -1,10 +1,37 @@
 import fs from "node:fs";
 
+function checkIndexForSymbol(i, j, data) {
+    // Assumes data has values at the provided indices
+    // i -> int
+    // j -> int
+    // data -> array[string]
+
+    const value = data[i][j];
+
+    if (!isNaN(value)) {
+        return false; // If the value is a number
+    }
+
+    if (value === ".") {
+        return false; // If the value is a period
+    }
+
+    return true; // Otherwise, true
+}
+
 fs.readFile("sample_input.txt", "utf8", (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
 
-    console.log(data); // Print out the file
+    const lines = data.split("\n");
+
+    for (let i = 0; i < lines.length; i++) {
+        for (let j = 0; j < lines[0].length; j++) {
+            // do something
+        }
+
+        console.log(lines[i]);
+    }
 })

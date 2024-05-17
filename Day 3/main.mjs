8f9ffbbe4e0acc80e0b1412878b041502bbdd9b9
Luna Karch <luna.karch05@gmail.com>
2024-05-17 09:19:22 -0400

    Added a function to check if the provided index exists in the data

--- Day 3/main.mjs
+++ Day 3/main.mjs
@@ -2,6 +2,7 @@ import fs from "node:fs";
 
 function checkIndexForSymbol(i, j, data) {
     // Assumes data has values at the provided indices
+
     // i -> int
     // j -> int
     // data -> array[string]
@@ -23,14 +24,34 @@ function getAdjacentIndices(i, jstart, jend, data) {
     // Will check surround indices for existance, if they exist
     // Checks returns an array of pairs of numbers, containing every
     // i and j around the selection provided
-    // i -> int
+
+    // i -> int : line number
     // jstart -> int : starting index
     // jend -> int : ending index
     // data -> array[string]
 
-    // Implement
+    for (let k = i - 1; k < i + 1; k++) {
+        for (let l = jstart - 1; l < jend + 1; l++) {
+
+        }
+    }
 }
 
+function checkIndicesExist(i, j, data) {
+    // Checks if the indices would be valid in the array of strings
+
+    // i -> int
+    // j -> int
+    // data -> array[string]
+
+    let iLeftBound = 0;
+    let iRightBound = data.length - 1;
+
+    let jLeftBound = 0;
+    let jRightBound = data[0].length - 1;
+
+    return iLeftBound <= i && i <= iRightBound && jLeftBound <= j && j <= jRightBound;
+}
 
 fs.readFile("sample_input.txt", "utf8", (err, data) => {
     if (err) {

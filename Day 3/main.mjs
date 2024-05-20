ca5690d4632d12aaf31949e578d1410f1ae4f15d
Luna Karch <luna.karch05@gmail.com>
2024-05-20 09:48:56 -0400

    Added a function to split the number indices so I can construct the numbers

--- Day 3/main.mjs
+++ Day 3/main.mjs
@@ -1,71 +1,62 @@
 import fs from "node:fs";
 
-function checkIndexForSymbol(i, j, data) {
-    // Assumes data has values at the provided indices
+function getNumberIndicesFromLine(line) {
+    // Returns an array of all the indices in a string that contain numbers
+    // line -> string
 
-    // i -> int
-    // j -> int
-    // data -> array[string]
+    let result = [];
 
-    const value = data[i][j];
-
-    if (!isNaN(value)) {
-        return false; // If the value is a number
-    }
-
-    if (value === ".") {
-        return false; // If the value is a period
-    }
-
-    return true; // Otherwise, true
-}
-
-function getAdjacentIndices(i, jstart, jend, data) {
-    // Will check surround indices for existance, if they exist
-    // Checks returns an array of pairs of numbers, containing every
-    // i and j around the selection provided
-
-    // i -> int : line number
-    // jstart -> int : starting index
-    // jend -> int : ending index
-    // data -> array[string]
-
-    for (let k = i - 1; k < i + 1; k++) {
-        for (let l = jstart - 1; l < jend + 1; l++) {
-
+    for (let i = 0; i < line.length; i++) {
+        if (!isNaN(line[i])) {
+            result.push(i);
         }
     }
+
+    return result;
 }
 
-function checkIndicesExist(i, j, data) {
-    // Checks if the indices would be valid in the array of strings
-
-    // i -> int
-    // j -> int
-    // data -> array[string]
-
-    let iLeftBound = 0;
-    let iRightBound = data.length - 1;
-
-    let jLeftBound = 0;
-    let jRightBound = data[0].length - 1;
-
-    return iLeftBound <= i && i <= iRightBound && jLeftBound <= j && j <= jRightBound;
-}
-
-fs.readFile("sample_input.txt", "utf8", (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
+function separateNumberIndices(numberIndices) {
+    if (numberIndices.length === 0) {
+        return [];
     }
 
-    const lines = data.split("\n");
+    let result = [];
+    let subArray = [numberIndices[0]];
 
-    for (let i = 0; i < lines.length; i++) {
-        for (let j = 0; j < lines[0].length; j++) {
-            // do something
+    for (let i = 1; i < numberIndices.length; i++) {
+        if (numberIndices[i] === numberIndices[i - 1] + 1) {
+            subArray.push(numberIndices[i]);
+        } else {
+            result.push(subArray);
+            subArray = [numberIndices[i]];
         }
-
-        console.log(lines[i]);
     }
-})
+
+    result.push(subArray);
+    return result;
+}
+
+
+function part1() {
+    fs.readFile("sample_input.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+        const lines = data.split("\n");
+    
+        let lineNumber = 0;
+        lines.forEach((line) => {
+            let numberIndices = getNumberIndicesFromLine(line);
+            console.log(separateNumberIndices(numberIndices));
+            lineNumber++;
+        })
+    })
+}
+
+function main() {
+    part1();
+}
+
+main();
